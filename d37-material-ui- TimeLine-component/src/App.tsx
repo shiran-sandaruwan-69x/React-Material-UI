e482839ed0ef0357c9be982f83a0bf42a624d5e9
shiran-sandaruwan-9x <sadaruwanshiran101@gmail.com>
2023-04-11 14:48:23 +0530

    coverd material-ui TimeLine component

---
+++ d37-material-ui- TimeLine-component/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import TimeLineComp from "./component/TimeLineComp";
+
+
+
+function App() {
+  return (
+
+          <div className="App">
+              <TimeLineComp/>
+          </div>
+
+  );
+}
+
+export default App;
